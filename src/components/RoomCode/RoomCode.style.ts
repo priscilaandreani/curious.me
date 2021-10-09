dc411632aafad612fc60b6e4db3a3bdcb5e0100b
priscila andreani <priscilaandreani@gmail.com>
2021-10-09 13:46:10 -0300

    feat: add roomCode component

---
+++ src/components/RoomCode/RoomCode.style.ts
@@ -0,0 +1,36 @@
+import styled from "styled-components";
+
+export const RoomCodeStyled =  styled.button`
+  display: flex;
+
+  overflow: hidden;
+  height: 40px;
+  border-radius: 8px;
+  
+  background-color: ${props => props.theme.colors.white};
+  border: 1px solid ${props => props.theme.colors.secundary};
+  cursor: pointer;
+
+  div{ 
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    height: 100%;
+
+    padding: 0 12px;
+    background-color: ${props => props.theme.colors.secundary};
+  }
+
+  span{ 
+    flex: 1;
+    display: block;
+    align-self: center;
+
+    width: 230px;
+    padding: 0 16px 0 12px;
+    
+    font-size: 14px;
+    font-weight: 500;
+  }
+`;
---
+++ src/components/RoomCode/RoomCode.tsx
@@ -0,0 +1,23 @@
+import { useCallback } from 'react';
+import copyImg from '../../assets/images/copy.svg';
+import { RoomCodeStyled } from './RoomCode.style';
+
+type RoomCodeProps = { 
+  code: string;
+}
+
+export function RoomCode({ code }: RoomCodeProps){
+
+  const copyToClipboard = useCallback(() => {
+    navigator.clipboard.writeText(code);
+  },[])
+
+  return(
+    <RoomCodeStyled onClick={copyToClipboard}>
+      <div>
+        <img src={copyImg} alt="Copiar" />
+      </div>
+      <span>{code}</span>
+    </RoomCodeStyled>
+  )
+}
