7dceada93253025f7429d127be56aa29b900a69b
priscila andreani <priscilaandreani@gmail.com>
2021-10-15 16:05:56 -0300

    fix(useRoom): remove catch errorObject

--- src/hooks/useRoom.tsx
+++ src/hooks/useRoom.tsx
@@ -43,36 +43,29 @@ export function useRoom(roomId: string) {
     // TODO: refactor event listener to optimize performance
     const roomRef = database.ref(`rooms/${roomId}`);
 
-    roomRef.on(
-      'value',
-      (room) => {
-        const databaseRoom = room.val();
-        const firebaseQuestions: FirebaseQuestions =
-          databaseRoom.questions ?? {};
+    roomRef.on('value', (room) => {
+      const databaseRoom = room.val();
+      const firebaseQuestions: FirebaseQuestions = databaseRoom.questions ?? {};
 
-        const parsedQuestions = Object.entries(firebaseQuestions).map(
-          ([key, value]) => {
-            return {
-              id: key,
-              content: value.content,
-              author: value.author,
-              isHighlighted: value.isHighlighted,
-              isAnswered: value.isAnswered,
-              likeCount: Object.values(value.likes ?? {}).length,
-              likeId: Object.entries(value.likes ?? {}).find(
-                ([key, like]) => like.authorId === user?.id
-              )?.[0],
-            };
-          }
-        );
+      const parsedQuestions = Object.entries(firebaseQuestions).map(
+        ([key, value]) => {
+          return {
+            id: key,
+            content: value.content,
+            author: value.author,
+            isHighlighted: value.isHighlighted,
+            isAnswered: value.isAnswered,
+            likeCount: Object.values(value.likes ?? {}).length,
+            likeId: Object.entries(value.likes ?? {}).find(
+              ([key, like]) => like.authorId === user?.id
+            )?.[0],
+          };
+        }
+      );
 
-        setTitle(databaseRoom.title);
-        setQuestions(parsedQuestions);
-      },
-      (errorObject: FirebaseQuestions) => {
-        console.log('The read failed: ' + errorObject.name);
-      }
-    );
+      setTitle(databaseRoom.title);
+      setQuestions(parsedQuestions);
+    });
 
     return () => {
       roomRef.off('value');
